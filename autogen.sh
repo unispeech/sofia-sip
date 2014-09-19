1cc7a7111a6959a6d639320f29c12a3eda955028
Arsen Chaloyan <achaloyan@gmail.com>
2014-09-18 17:50:49 -0700

    Use libtoolize in autogen.sh to fix errors regarding LIBTOOL encountered with autoconf 2.63. Also, use default automake and aclocal. Before, automake-1.9 was used.

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+case `uname` in
+    Darwin) libtoolize=glibtoolize ;;
+    *)      libtoolize=libtoolize  ;;
+esac
+
 set -x
-AUTOMAKE=${AUTOMAKE:-automake-1.9} ACLOCAL=${ACLOCAL:-aclocal-1.9}
+AUTOMAKE=${AUTOMAKE:-automake} ACLOCAL=${ACLOCAL:-aclocal}
 export AUTOMAKE ACLOCAL
+$libtoolize --force --automake --copy
 ${AUTORECONF:-autoreconf} -i
 find . \( -name 'run*' -o -name '*.sh' \) -a -type f | xargs chmod +x
 chmod +x scripts/*
